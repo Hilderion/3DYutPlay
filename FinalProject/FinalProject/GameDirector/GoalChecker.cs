238f50929c72037e02388d07be4e25f6087a5142
KCCI <KCCI@KCCI-PC>
2018-05-31 11:39:43 +0900

    add GoalChecker

--- FinalProject/FinalProject/GameDirector/GoalChecker.cs
+++ FinalProject/FinalProject/GameDirector/GoalChecker.cs
@@ -8,5 +8,47 @@ namespace FinalProject.GameDirector
 {
     class GoalChecker
     {
+        public int _player1GoalCount = 0;
+        public int _player2GoalCount = 0;
+
+        #region playerUnits
+        Unit _player1UnitKnight = new Unit(1, UnitTag.Knight);
+        Unit _player1UnitHeavySoldier = new Unit(1, UnitTag.HeavySoldier);
+        Unit _player1UnitGuard = new Unit(1, UnitTag.Guard);
+        Unit _player1UnitScout = new Unit(1, UnitTag.Scout);
+
+        Unit _player2UnitKnight = new Unit(2, UnitTag.Knight);
+        Unit _player2UnitHeavySoldier = new Unit(2, UnitTag.HeavySoldier);
+        Unit _player2UnitGuard = new Unit(2, UnitTag.Guard);
+        Unit _player2UnitScout = new Unit(2, UnitTag.Scout);
+        #endregion
+
+        public void GoalCheck()
+        {
+            if (_player1UnitScout.isGoaled == true
+                || _player1UnitHeavySoldier.isGoaled == true
+                || _player1UnitGuard.isGoaled == true
+                || _player1UnitScout.isGoaled == true)
+                _player1GoalCount++;
+
+            if (_player2UnitScout.isGoaled == true
+                || _player2UnitHeavySoldier.isGoaled == true
+                || _player2UnitGuard.isGoaled == true
+                || _player2UnitScout.isGoaled == true)
+                _player2GoalCount++;
+        }
+
+        public void EndCheck()
+        {
+            if (_player1GoalCount >= 4)
+            {
+                // TODO : 플레이어 1 게임 승리 구현
+            }
+
+            if (_player2GoalCount >= 4)
+            {
+                // TODO : 플레이어 2 게임 승리 구현
+            }
+        }
     }
 }
