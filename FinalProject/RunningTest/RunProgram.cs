27f93f08c4126f54913f60f8d39ea519b306453e
Hilde <skullreader@naver.com>
2018-06-04 16:30:35 +0900

    종합됨

--- FinalProject/RunningTest/RunProgram.cs
+++ FinalProject/RunningTest/RunProgram.cs
@@ -18,13 +18,14 @@ namespace FinalProject.RunningTest
 
             List<double> testBot1 = testUnit.TestBot();
             List<double> testBot2 = testUnit.TestBot2();
-
+            // 19-20에 새로 각 플레이어의 유닛을 추가하고
 
             TurnChecker actualGame = new TurnChecker();
 
             for (int i = 0; i < 2; i++)
             {
                 actualGame.Player1Turn(testBot1,testBot2);
+                // 27에 플레이어1,플레이어2를 넣으면 구동한다.
         
             }
 
