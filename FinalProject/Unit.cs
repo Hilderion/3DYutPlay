cb6a9c4e310c114329ec48c16a3119f699d6dcd5
KCCI <kabalah_qbl@naver.com>
2018-06-14 10:17:54 +0900

    Move 메소드는 미완입니다

--- FinalProject/Unit.cs
+++ FinalProject/Unit.cs
@@ -4,20 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalProject
+namespace ConsoleApp2
 {
     class Unit
     {
         private int Attackpoint { get; set; }
         private int Defencepoint { get; set; }
         private int Extramovepoint { get; set; }
+        private bool isGoaled = false;
+        public int[] Location = new int[3];
+        private int[] FormalLocation = new int[3];
+        public List<Unit> units;
 
-        public void Move() { }
-        public void Battle(Unit unit) { }
-        public void UnitLink(Unit unit) { }
-        public void ChangeAttackPoint(int point) { }
-        public void ChangeDefencePoint(int point) { }
-        public int ChangeMovePoint(int point) { return Extramovepoint; }
-        public void GoalUnit() { }
+        public Unit(int attack, int Defence, int ExMove)
+        {
+            Attackpoint = attack;
+            Defencepoint = Defence;
+            Extramovepoint = ExMove;
+
+            units = new List<Unit>();
+            units.Add(this);
+        }
+
+        public void Move(int yut)
+        {
+            if (yut == 6)
+            {
+
+            }
+        }
+        public void Battle(Unit defence) //이 메소드의 주체가 공격측, 매개변수 defense가 방어측
+        {
+            if (units.Max(x => x.Attackpoint) > defence.units.Max(x => x.Defencepoint))
+            {
+                foreach (Unit dead in defence.units)
+                {
+                    dead.Location = new int[3] { 0, 0, 0 };
+                    dead.FormalLocation = new int[3] { 0, 0, 0 };
+                }
+            }
+            else
+            {
+                Move(6);
+            }
+        }
+
+        public void UnitLink(Unit unit)
+        {
+            units.AddRange(unit.units);
+            unit.units.Clear();
+        }
+
+        public void ChangeAttackPoint(int point)
+        {
+            foreach (Unit unit in units)
+            {
+                Attackpoint = point;
+            }
+        }
+
+        public void ChangeDefencePoint(int point)
+        {
+            foreach (Unit unit in units)
+            {
+                Defencepoint = point;
+            }
+        }
+
+        public void ChangeMovePoint(int point)
+        {
+            foreach (Unit unit in units)
+            {
+                Extramovepoint = point;
+            }
+        }
+
+        public void GoalUnit()
+        {
+            isGoaled = true;
+        }
     }
 }
