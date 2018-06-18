3b84fa27d54cc50717ff61f3cf41b8da4a23a6d1
thkim <cplkimth@naver.com>
2018-06-18 15:03:28 +0900

    .

---
+++ Game/Assets/Scripts/GameDirector.cs
@@ -0,0 +1,44 @@
+﻿using FinalProject;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+
+    // Use this for initialization
+	void Start () {
+	    Game.Instance.HorseMoved += Game_HorseMoved;
+
+        Game.Instance.FieldPointVisited += Game_FieldPointVisited;
+
+        while (true)
+        {
+            int dice = Yut.Instance.Roll();
+
+            
+        }
+	}
+
+    private void Game_FieldPointVisited(object sender, Game.FieldPointVisitedEventArgs e)
+    {
+        GameObject horse = GameObject.Find("Horse" + e.Horse.Id);
+        GameObject point = GameObject.Find("Point" + e.FieldPoint.Id);
+
+        MoveHorse(horse, point);
+    }
+
+    private void MoveHorse(GameObject horse, GameObject point)
+    {
+        // 말을 부드럽게 이동
+    }
+
+    private void Game_HorseMoved(object sender, Game.HorseMovedEventArgs e)
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
